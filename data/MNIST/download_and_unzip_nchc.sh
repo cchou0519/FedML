d7de8a11ce678daec4c1d8b520912b52c101a2cb
cchou0519 <73981474+cchou0519@users.noreply.github.com>
2021-07-05 10:50:57 +0800

    Update download_and_unzip_nchc.sh

--- data/MNIST/download_and_unzip_nchc.sh
+++ data/MNIST/download_and_unzip_nchc.sh
@@ -1,11 +1,9 @@
 rm -rf mnist_data
+     
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet \
+--save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1hLDejqH5eg-cCAjtkXgnkmuFPW5AQsos' -O- \
+| sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1hLDejqH5eg-cCAjtkXgnkmuFPW5AQsos" -O mnist_data.zip && rm -rf /tmp/cookies.txt
 
-export fileid=1hLDejqH5eg-cCAjtkXgnkmuFPW5AQsos
-export filename=mnist_data.zip
-wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
-     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
-wget --load-cookies cookies.txt -O $filename \
-     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
 unzip mnist_data.zip
 
 rm -rf mnist_data.zip
